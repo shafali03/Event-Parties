9b177087c194f2d3abc7d80008d4486616d77180
shafali03 <shaf03sm@gmail.com>
2020-06-21 20:50:22 +0100

    feat: Display mock events

--- app.js
+++ app.js
@@ -8,9 +8,21 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 
 app.set('view engine', 'handlebars');
 
-// send hello world message to home page
+// send message to home page
 app.get('/', (req, res) => {
   res.render('home', { msg: 'Handlebars are cool' });
+})
+
+// mock array
+var events = [
+  { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://cdn.pixabay.com/photo/2016/04/18/22/05/sea-1337565__480.jpg" },
+  { title: "I am your second event", desc: "A great event that is super fun to look at and good", imgUrl: "https://cdn.pixabay.com/photo/2017/08/08/00/17/events-2609526__480.jpg" },
+  { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://cdn.pixabay.com/photo/2017/08/08/00/17/events-2609526__480.jpg" }
+]
+
+// index
+app.get('/events', (req, res) => {
+  res.render('events-index', { events: events })
 })
 
 // choose port 3000
@@ -20,5 +32,4 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('Listening to port 3000')
 })
-
 
